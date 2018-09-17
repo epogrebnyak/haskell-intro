aff24052146368b9603af626544971b94853c098
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2018-09-17 14:01:52 +0300

    Update addition.hs

--- class1/addition.hs
+++ class1/addition.hs
@@ -1,3 +1,14 @@
+-- Trying to show:
+--    - define function and function type in signature
+--    - currying
+--    - currying
+-- Not shown:
+--    - lambdas
+--    - pattern mathcing
+--    - different ways to define a fucntions on several types
+--    - let in / where / do 
+--    - multiline statement?
+
 addTwo :: Int -> Int -> Int
 addTwo a b = a + b 
 
@@ -13,14 +24,15 @@ runFunc f = f 2
 square :: Double -> Double
 square x = x * x 
 
-
--- multiline statement?
-
-
+-- Additions:
 -- https://stackoverflow.com/questions/46547208/why-is-my-haskell-code-saying-variable-not-in-scope-main
-
 -- supress warnings
+
 main = return ()
 
 -- assertions
 -- https://downloads.haskell.org/~ghc/6.12.2/docs/html/users_guide/assertions.html
+
+-- Good functions tutorial
+-- http://www.toves.org/books/hsfun/
+-- but lacks lambda fucntions
