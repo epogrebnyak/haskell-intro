05c72b77af019affbed449d02b559f8574b9f20f
E Pogrebnyak <e.pogrebnyak@gmail.com>
2018-10-04 10:35:05 +0300

    minor edit

--- class4/home.hs
+++ class4/home.hs
@@ -2,8 +2,9 @@
 {-
 
 * make a pretty printer to expression language from class2 home work
+
 * define your own versions for Sum and Product monoids.
   Sum uses 0 as a zero element and + as an operation,
-  and Pruduct uses 1 and * for them.
+  and Product uses 1 and * for them.
 
 -}
