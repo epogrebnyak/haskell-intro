db2a2487b67d6e7befda124ca3de44f9a36f501c
E Pogrebnyak <e.pogrebnyak@gmail.com>
2018-10-08 17:22:20 +0300

    monoid attempts

--- class4/home_ep.hs
+++ class4/home_ep.hs
@@ -11,13 +11,81 @@
 
 -- Mononid refence
 -- https://en.wikibooks.org/wiki/Haskell/Monoids
+
+
+-- One idea is:  
 class MySum a where
     empty :: a
     plus :: a -> a -> a
 
+-- does this make sense?    
 instance MySum Int where
     empty = 0
     plus a b = a + b
-    
--- how do I use it? 
+
+-- seems like I need a function to start using a new class
+-- (confusion: cannot just instativate type )
+addTwo 0 b = b
+addTwo a 0 = a
+addTwo a b = plus a b
+
 -- how is `<>` overloaded?    
+(p) a b = plus a b 
+
+-- Another idea is make class    
+
+{-
+http://www.scs.stanford.edu/14sp-cs240h/slides/phantoms.html
+
+# Monoids for multiplication
+newtype Product a = Product { getProduct :: a }
+    deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Num a => Monoid (Product a) where
+    mempty                        = Product 1
+    Product x `mappend` Product y = Product (x * y)
+
+# Monoids for addition
+newtype Sum a = Sum { getSum :: a }
+    deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Num a => Monoid (Sum a) where
+    mempty                = Sum 0
+
+    Sum x `mappend` Sum y = Sum (x + y)
+
+-}
+
+{-
+-- Also semigroup package
+-- http://hackage.haskell.org/package/base-4.12.0.0/docs/src/Data.Semigroup.Internal.html#Sum
+
+-- | @since 2.01
+instance Monoid Any where
+        mempty = Any False
+
+-- | Monoid under addition.
+--
+-- >>> getSum (Sum 1 <> Sum 2 <> mempty)
+-- 3
+newtype Sum a = Sum { getSum :: a }
+        deriving ( Eq       -- ^ @since 2.01
+                 , Ord      -- ^ @since 2.01
+                 , Read     -- ^ @since 2.01
+                 , Show     -- ^ @since 2.01
+                 , Bounded  -- ^ @since 2.01
+                 , Generic  -- ^ @since 4.7.0.0
+                 , Generic1 -- ^ @since 4.7.0.0
+                 , Num      -- ^ @since 4.7.0.0
+                 )
+
+-- | @since 4.9.0.0
+instance Num a => Semigroup (Sum a) where
+        (<>) = coerce ((+) :: a -> a -> a)
+        stimes n (Sum a) = Sum (fromIntegral n * a)
+
+-- | @since 2.01
+instance Num a => Monoid (Sum a) where
+        mempty = Sum 0
+
+-}
