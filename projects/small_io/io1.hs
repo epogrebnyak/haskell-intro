71fb0ebf82008f87ee9256c0e1c9673c997bdf74
epogrebnyak <e.pogrebnyak@gmail.com>
2018-11-07 08:21:26 +0300

    attempt to parse CSV file

---
+++ projects/small_io/io1.hs
@@ -0,0 +1,57 @@
+--main :: IO ()
+--main = do
+--  contents <- B.getContents
+--  B.putStr contents
+
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as B
+import Data.Vector (Vector)
+import Data.Csv 
+
+
+decode' :: BL.ByteString -> Either String (Vector (Vector BL.ByteString))
+decode' = decode NoHeader 
+getBytes filename = BL.readFile filename --"1.txt"
+
+main :: IO ()
+main = do
+  contents <- getBytes
+  BL.putStr contents  
+
+-- This works:
+-- getBytes >>= BL.putStr
+
+
+-- MINOR: Can this be helpful?  
+-- newtype Matrix = Vector (Vector BL.ByteString)
+
+-- TODO: I want a function that takes a filename or getBytes
+--       and returns a Vector (Vector BL.ByteString)    
+
+getVectorOfVectors :: String -> Either String (Vector (Vector BL.ByteString))
+
+{--
+
+Treating CSV data as opaque byte strings
+
+Sometimes you might want to work with a CSV file which contents is unknown to you. For example, you might want remove the second column of a file without knowing anything about its content. To parse a CSV file to a generic representation, just convert each record to a Vector ByteString value, like so:
+
+>>> import Data.ByteString (ByteString)
+>>> decode NoHeader "John,27\r\nJane,28\r\n" :: Either String (Vector (Vector ByteString))
+Right [["John","27"],["Jane","28"]]
+
+As the example output above shows, all the fields are returned as uninterpreted ByteString values.
+
+--}
+
+
+-- Reference
+-- > :t getBytes
+-- getBytes :: IO BL.ByteString
+
+-- :t BL.putStr
+-- BL.putStr :: BL.ByteString -> IO ()
+
+-- :t (>>=)
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
