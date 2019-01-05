a39f353f6f361a1cd012dcc201e4a2df27499bc1
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-01-05 13:46:29 +0300

    advances on table parsing

--- projects/tables/extract_datapoints.hs
+++ projects/tables/extract_datapoints.hs
@@ -23,12 +23,6 @@ data Datapoint = Datapoint {
     }    
     deriving (Show)
 
-h1 = [["GDP"], ["% change to year earlier"]]       
-d1 = [["2017","100,6","102,5","102,2","100,9"], ["2018","101,3","101,9","101,5",""]]
-t1 = Table h1 d1
-p1 = Datapoint "GDP" "bln_rub" 2017 Nothing (Annual::Frequency) 60000
-p2 = Datapoint {name = "GDP", unit = "bln_rub", year = 2017, month = Nothing, freq = Annual, value = 60000}  
-
 splitRow :: RowFormat -> Row -> [Datapoint]
 splitRow "qqqq" row = [p1, p1, p2, p2] where year = head(row)
 
@@ -55,4 +49,10 @@ getValues t =
     -- partial application, splitter will expect a row as an argument
     where splitter = splitRow (getFormat t)
 
+
+h1 = [["GDP"], ["% change to year earlier"]]       
+d1 = [["2017","100,6","102,5","102,2","100,9"], ["2018","101,3","101,9","101,5",""]]
+t1 = Table h1 d1
+p1 = Datapoint "GDP" "bln_rub" 2017 Nothing (Annual::Frequency) 60000
+p2 = Datapoint {name = "GDP", unit = "bln_rub", year = 2017, month = Nothing, freq = Annual, value = 60000}  
 vs = getValues(t1)    
---
+++ projects/tables/task.hs
@@ -0,0 +1,31 @@
+type RowFormat = [Char]
+expand :: RowFormat -> [(Char, Int)]
+expand pat = [(c, count c (take (i+1) pat)) | (c, i) <- zip pat [0..]]
+
+
+msg a word b = (show a) ++ " " ++ word ++ " " ++ (show b)
+assertEquals' a b = if a /= b then error $ msg a "not equal to" b 
+                              else putStrLn "Passed"
+
+
+data Value = Value {
+    freq:: Char,
+    year:: Int,
+    period:: Int,
+    x:: Float
+} deriving Show
+
+row = [100, 10, 40, 25, 25]
+fmt = "aqqqq"
+expected = [('a',1,100),('q',1,10),('q',2,40),('q',3,25),('q',4,25)]
+
+count :: Char -> String -> Int
+count letter str = length $ filter (== letter) str
+
+split :: String -> [a] -> [(Char, Int, a)]
+split fmt row = [(freq, count freq (fmt' i), x)   
+               | (freq, x, i) <- zip3 fmt row [0..]]
+               where fmt' i = take (i+1) fmt
+
+-- split "aqqqq" [100, 10, 40, 25, 25]
+-- [('a',1,100),('q',1,10),('q',2,40),('q',3,25),('q',4,25)]
