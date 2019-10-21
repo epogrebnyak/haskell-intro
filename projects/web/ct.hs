6887dba2faf605361c397f71691749efdfbdf590
Evgeniy <e.pogrebnyak@gmail.com>
2019-10-21 21:24:32 +0300

    some copy-paste

--- projects/web/ct.hs
+++ projects/web/ct.hs
@@ -1,8 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Aeson            (Value)
-import qualified Data.ByteString.Char8 as S8
-import qualified Data.Yaml             as Yaml
-import           Network.HTTP.Simple
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Simple -- http-conduit
+
+main :: IO ()
+main = do
+    response <- httpLBS "http://httpbin.org/get"
+
+    putStrLn $ "The status code was: " ++
+               show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    L8.putStrLn $ getResponseBody response
+
+-- import           Data.Aeson            (Value)
+-- import qualified Data.ByteString.Char8 as S8
+
+-- import qualified Data.Yaml             as Yaml
+-- import           Network.HTTP.Simple
 
 -- main :: IO ()
 -- main = do
