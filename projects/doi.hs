c1ac0735898ed839e197ef113c46c30ac9553c38
Evgeniy <e.pogrebnyak@gmail.com>
2019-10-21 16:56:34 +0300

    so link

--- projects/doi.hs
+++ projects/doi.hs
@@ -1,9 +1,10 @@
+-- https://stackoverflow.com/questions/21371316/reading-file-from-website-in-haskell
+
 -- wreq and http-client-tls support HTTPS out of the box. YMMV. There's a reason I don't really recommend older Haskell libraries even if they're maintained. The foundation of many libraries is http-client and it's a pretty popular library to use. It's used in http-conduit and pipes-http as well. The latter of which is a single 130 line module that has required almost zero maintenance in the past two years to add pipes streaming support to http-client. Things that use http-client are generally nice but you'll often want to use something higher level than http-client itself, such as wreq.
 -- https://bitemyapp.com/blog/haskell-is-not-trivial-not-unfair/  
 
--- a struggle to install
+-- a struggle to install http-conduit
 -- https://stackoverflow.com/questions/13200580/haskell-could-not-find-module-network-http-conduit
-
 
 
 -- import Network.HTTP
@@ -17,7 +18,3 @@ import Network.Wreq
 
 main = do 
   r <- get "http://httpbin.org/get"
-  print (r ^. responseBody)
-
--- stumble block - cannot run cabal update on WSL
-
