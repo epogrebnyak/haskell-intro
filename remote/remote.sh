067d32d49986b1c135ed9485413673eff237c525
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2019-10-22 16:30:01 +0300

    Update remote.sh

--- remote/remote.sh
+++ remote/remote.sh
@@ -1,5 +1,8 @@
+# provision an instance with at least 2GB memory, AWS commandline
 sudo apt-get update
 sudo apt-get install haskell-platform
+cabal update
+cabal install aeson
 mkdir ~/github
-cd ~/github
-git clone https://github.com/epogrebnyak/haskell-intro.git
+git clone https://github.com/epogrebnyak/haskell-intro.git ~/github/haskell-intro
+# make an AMI/template for this installation
