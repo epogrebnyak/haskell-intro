d09b2500a6b6f40f0458c22a4dbcc6bf673b37a9
Ubuntu <ubuntu@ip-172-31-19-98.eu-central-1.compute.internal>
2019-10-22 14:12:04 +0000

    decomposition of req example

--- projects/web/req.hs
+++ projects/web/req.hs
@@ -6,18 +6,17 @@ import Control.Monad.IO.Class
 import Data.Aeson
 import Network.HTTP.Req
 
+payload = ReqBodyJson $ object
+        [ "foo" .= (10 :: Int)
+        , "bar" .= (20 :: Int) ]
+
 main :: IO ()
 -- You can either make your monad an instance of 'MonadHttp', or use
 -- 'runReq' in any IO-enabled monad without defining new instances.
 main = runReq defaultHttpConfig $ do
-  let payload = object
-        [ "foo" .= (10 :: Int)
-        , "bar" .= (20 :: Int) ]
-  -- One function—full power and flexibility, automatic retrying on timeouts
-  -- and such, automatic connection sharing.
   r <- req POST -- method
     (https "httpbin.org" /: "post") -- safe by construction URL
-    (ReqBodyJson payload) -- use built-in options or add your own
+    payload      -- use built-in options or add your own
     jsonResponse -- specify how to interpret response
     mempty       -- query params, headers, explicit port number, etc.
   liftIO $ print (responseBody r :: Value)
