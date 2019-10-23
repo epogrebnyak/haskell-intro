945243b9ec21fe4ef8f75210b40acfce716a4934
Evgeniy <e.pogrebnyak@gmail.com>
2019-10-23 10:48:36 +0300

    curl library

--- remote/remote.sh
+++ remote/remote.sh
@@ -1,11 +1,16 @@
 # provision an instance with at least 2GB memory, maybe from AWS command line
+# in the instance:
 sudo apt-get update
 sudo apt-get install haskell-platform
+# global installations are not good, but this is a try
 cabal update
 cabal install aeson
 cabal install http-client
 cabal install http-client-tls
 cabal install http-conduit
 cabal install req --force-reinstalls
+sudo apt-get install libcurl4-gnutls-dev
+cabal install curl
+# download and use the excercise repo
 mkdir ~/github
 git clone https://github.com/epogrebnyak/haskell-intro.git ~/github/haskell-intro
