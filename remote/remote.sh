b524082a6e211881033d5cba5f772d68f0dd8bed
Evgeniy <e.pogrebnyak@gmail.com>
2019-10-23 10:52:16 +0300

    git password

--- remote/remote.sh
+++ remote/remote.sh
@@ -2,7 +2,7 @@
 # in the instance:
 sudo apt-get update
 sudo apt-get install haskell-platform
-# global installations are not good, but this is a try
+# global installations for cabal are not good, but this is a try
 cabal update
 cabal install aeson
 cabal install http-client
@@ -14,3 +14,6 @@ cabal install curl
 # download and use the excercise repo
 mkdir ~/github
 git clone https://github.com/epogrebnyak/haskell-intro.git ~/github/haskell-intro
+# cache git password
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
