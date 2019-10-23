f92f56b96ea9680ae76ef3dd34f712babaa14427
epogrebnyak <ubuntu@ip-172-31-19-98.eu-central-1.compute.internal>
2019-10-23 17:07:39 +0000

    doi.hs downloads bibliography

--- projects/doi/doi.hs
+++ projects/doi/doi.hs
@@ -2,7 +2,7 @@
 -- https://github.com/nushio3/citation-resolve/blob/master/src/Text/CSL/Input/Identifier/Internal.hs#L142-L152
 -- Requires sudo apt-get install libcurl4-gnutls-dev
 
-
+-- Source code:
 -- | resolvers for specific document IDs.
 -- resolveDOI :: MonadIO m => RM m String String
 -- resolveDOI docIDStr = do
@@ -14,31 +14,55 @@
 --   bs <- liftIOE $ openURIWithOpts opts url
 --   return $ BS.unpack bs
 
+{-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Char8 as B
+import Data.Text (pack, unpack, replace, append, toLower)
 import Network.Curl.Download (openURIWithOpts)
 import Network.Curl.Opts (CurlOption(CurlFollowLocation, CurlHttpHeaders))
 
 type URL = String
+data Handle = DOI String | ISBN String deriving Show
 
-
-get :: URL -> IO (Either String B.ByteString)
-get url = openURIWithOpts opts url where 
+fromWeb :: URL -> IO (Either String B.ByteString)
+fromWeb url = openURIWithOpts opts url where 
     opts = [ CurlFollowLocation True,
              CurlHttpHeaders ["Accept: application/vnd.citationstyles.csl+json"]
            ]    
 
---save :: FilePath -> IO (Either String B.ByteString) -> IO ()           
+save :: FilePath -> Either String B.ByteString -> IO ()           
 save filename (Right content) = writeFile filename (B.unpack content)
--- save filename (Left _) = return 
+save filename (Left _) = return () -- do nothing
 
--- TODO:
--- saveURL :: URL -> FilePath -> IO()
--- saveURL url filename = (get url) >>= (save filename)
+url :: Handle -> URL
+url (DOI s) = "https://doi.org/" ++ s
+url (ISBN s) = "https://isbn.org/" ++ s -- not real url
 
-url = "https://doi.org/10.3982/ECTA11427"
-a = get "https://doi.org/10.3982/ECTA11427"
+str :: Handle -> String
+str (DOI s) = "doi:" ++ s
+str (ISBN s) = "isbn:" ++ s
 
-main = do 
-    a >>= print
-    a >>= (save "10.3982_ECTA11427.txt")
+filename :: Handle -> FilePath
+filename h = (unpack . changes . pack . str) h
+      where changes = ( toLower . (\x -> append x ".txt") . 
+                                  (replace "." "_") . 
+                                  (replace "/" "_") . 
+                                  (replace ":" "_") )
+       
+load :: Handle -> IO()
+load h = (fromWeb (url h)) >>= (save (filename h))
 
+-- TODO: must read file the local file if it exists
+--       and query web and save if it does not
+--       return value in any case
+get h = readFile $ filename h
+
+h = DOI "10.3982/ECTA11427"
+
+main = load $ DOI "10.3982/ECTA11427"
+
+-- NEXT: make sample output using python program
+-- NEXT: say what python program does not do
+-- NEXT: read a local file as JSON or CSL item
+-- NEXT: correct csl item so that it fits pandoc-cslproc
+-- NEXT: make bibliography
+-- NEXT: make it a small package
