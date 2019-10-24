cb0ff4fcfd673870b910990bcadd1998d9ac384f
Evgeniy <e.pogrebnyak@gmail.com>
2019-10-24 10:31:11 +0300

    retrieveDOI

--- projects/doi/doi.hs
+++ projects/doi/doi.hs
@@ -1,55 +1,49 @@
 -- Replicate calls to bibliography server
--- https://github.com/nushio3/citation-resolve/blob/master/src/Text/CSL/Input/Identifier/Internal.hs#L142-L152
--- Requires sudo apt-get install libcurl4-gnutls-dev
 
--- Source code:
--- | resolvers for specific document IDs.
--- resolveDOI :: MonadIO m => RM m String String
--- resolveDOI docIDStr = do
---   let
---       opts = [ CurlFollowLocation True
---              , CurlHttpHeaders ["Accept: text/bibliography; style=bibtex"]
---              ]
---       url = "http://dx.doi.org/" ++ docIDStr
---   bs <- liftIOE $ openURIWithOpts opts url
---   return $ BS.unpack bs
+-- Similar package (outdated bibliographic APIs, but does nocal caching nicely)
+-- https://github.com/nushio3/citation-resolve/blob/master/src/Text/CSL/Input/Identifier/Internal.hs#L142-L152
+
 
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Char8 as B
-import Data.Text (pack, unpack, replace, append, toLower)
+import Data.Text (pack, unpack, replace, toLower)
 import Network.Curl.Download (openURIWithOpts)
 import Network.Curl.Opts (CurlOption(CurlFollowLocation, CurlHttpHeaders))
 
 type URL = String
 data Handle = DOI String | ISBN String deriving Show
 
-fromWeb :: URL -> IO (Either String B.ByteString)
-fromWeb url = openURIWithOpts opts url where 
+curl :: URL -> CurlHttpHeaders -> IO (Either String B.ByteString)
+curl url header = openURIWithOpts opts url where 
     opts = [ CurlFollowLocation True,
-             CurlHttpHeaders ["Accept: application/vnd.citationstyles.csl+json"]
+             CurlHttpHeaders header
            ]    
 
 save :: FilePath -> Either String B.ByteString -> IO ()           
 save filename (Right content) = writeFile filename (B.unpack content)
 save filename (Left _) = return () -- do nothing
 
-url :: Handle -> URL
-url (DOI s) = "https://doi.org/" ++ s
-url (ISBN s) = "https://isbn.org/" ++ s -- not real url
+retrieveDOI s = curl url header
+    where url = "https://doi.org/" ++ s 
+          header = CurlHttpHeaders ["Accept: application/vnd.citationstyles.csl+json"]   
+
+retrieve (DOI s) = retrieveDOI s
+retrieve _ = return (Left "Not supported")
 
 str :: Handle -> String
 str (DOI s) = "doi:" ++ s
 str (ISBN s) = "isbn:" ++ s
 
 filename :: Handle -> FilePath
-filename h = (unpack . changes . pack . str) h
-      where changes = ( toLower . (\x -> append x ".txt") . 
-                                  (replace "." "_") . 
-                                  (replace "/" "_") . 
-                                  (replace ":" "_") )
+filename h = ((unpack . changes . pack . str) h) + ".txt" 
+      where changes = (toLower . 
+                       (replace "." "_") . 
+                       (replace "/" "_") . 
+                       (replace ":" "_")
+                       )
        
-load :: Handle -> IO()
-load h = (fromWeb (url h)) >>= (save (filename h))
+loadLocal :: Handle -> IO()
+loadLocal h = (retrieve h) >>= (save (filename h))
 
 -- TODO: must read file the local file if it exists
 --       and query web and save if it does not
