227ba4d88154d5a724c20d787ba93873a6849d77
Ubuntu <ubuntu@ip-172-31-28-233.eu-central-1.compute.internal>
2020-01-07 21:10:01 +0000

    try using pandoc

--- pandoc-example/src/Lib.hs
+++ pandoc-example/src/Lib.hs
@@ -1,6 +1,16 @@
+-- https://ocharles.org.uk/guest-posts/2013-12-12-24-days-of-hackage-pandoc.htm
+
 module Lib
     ( someFunc
     ) where
 
+import Text.Pandoc
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+textToConvert = unlines [ "Hello World."
+                        , ""
+                        , "    this is a Markdown code block"
+                        , ""
+                        , "[This is a link](http://www.latermuse.com/)" ]
