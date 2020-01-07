760993aa4e108f29a682d49011e5cee09ede8c1f
Ubuntu <ubuntu@ip-172-31-28-233.eu-central-1.compute.internal>
2020-01-07 21:15:53 +0000

    more pandoc examples

--- pandoc-example/src/Lib.hs
+++ pandoc-example/src/Lib.hs
@@ -6,11 +6,23 @@ module Lib
 
 import Text.Pandoc
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
 textToConvert = unlines [ "Hello World."
                         , ""
                         , "    this is a Markdown code block"
                         , ""
                         , "[This is a link](http://www.latermuse.com/)" ]
+
+pandocParsed = readMarkdown def textToConvert
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+pandocConverted = writeLaTeX def pandocParsed
+
+convertedToHtml = writeHtml def pandocParsed
+
+convertedToOpenDocument = writeOpenDocument opts pandocParsed
+  where
+    opts = def { writerWrapText = True -- Enable text wrapping
+               , writerColumns = 80 }  -- Set column width to 80
+
